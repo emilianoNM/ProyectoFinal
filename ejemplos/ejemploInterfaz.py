934437510c95adadd971a22ca09aa18d02e3c168
Emiliano Nava Morales <enava@asimov.mx>
2018-04-23 08:45:37 -0500

    Interfaz

---
+++ ejemplos/ejemploInterfaz.py
@@ -0,0 +1,16 @@
+class Persona():
+    def  Acceso(self,usuario,contrase):
+
+        return  "11111123"
+    def pago(self,producto,cantidad,metodoPago)
+
+    if ( ):
+        return "111111"
+   else():
+     return "erro de pago "
+
+ def reclamacion(self,idCompra):
+     if(idCompra==0):
+        return "No existe la compra"
+     else
+        return Compra(1,1,1)
