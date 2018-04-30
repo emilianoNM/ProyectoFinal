2022d2b4de232da3e32dde714e9624368275a3ee
LauraG18 <36383275+LauraG18@users.noreply.github.com>
2018-04-30 00:12:14 -0500

    Add files via upload

---
+++ Productos/Estructua base de clase/ClaseCliente.py
@@ -0,0 +1,48 @@
+#Estructura básica de clases
+#Clase Cliente
+
+import ClasePersona
+
+class Cliente(Persona):
+	Usuario=None
+	Contraseña=None
+	Producto=None
+	Pregunta=None
+	Tarjeta=None
+	Pago=None
+	Entrega=None 
+
+def __init__(self,Usuario,Contraseña,Producto,Pregunta,Tarjeta,Pago,Entrega)
+		self.Usuario=Usuario
+		self.Contraseña=Contraseña
+		self.Producto=Producto
+		self.Pregunta=Pregunta
+		self.Tarjeta=Tarjeta
+		self.Pago=Pago
+		self.Entrega=Entrega 
+	
+def Crear_Usuario(self):
+	print("Crea tu cuenta para continuar con la compra")
+	Ent=str(input("Usuario: \t E-mail: \t Telefono:  \t Contraseña: \t")
+	
+def Buscar_Producto(self):
+	print("Puedes seleccionar varios productos\t ")
+	print("Introduce la cantidad de productos que necesites")
+	print("Solo das click y va directo a tu carrito")
+	P=int(input("Cantidad: \t")
+
+def Hacer_Pregunta(self):
+	print("Puedes hacer una pregunta acerca del producto de tu interes")
+	Pre=("Pregunta: \t Enviar \t")
+def Realizar_Pedido(self):
+
+def Elegir_Pago(self):
+	print("Entrar metodo de pago")
+#Opciones tarjeta credito, debito, efectivo, paypal, deposito, etc
+def Elegir_Entrega(self):
+	print("Entrar lugar y metodo de entrega")
+	D=("Ubicacion de entrega: \t ")
+def Calificar_Entrega(self):
+	print("¿Llego a tiempo tu producto? \t ¿Fue lo que esperabas? \t Quejas y sugerencias \t")
+
+	
---
+++ Productos/Estructua base de clase/ClasePersona.py
@@ -0,0 +1,16 @@
+#Estructura basica de clases
+#Clase Persona
+
+class Persona(object):
+  def  __init__(self,id,nombre,edad,e-mail,telefono,celular,rfc,direccion):
+        self.id=id
+        self.nombre=nombre
+	self.edad=edad
+	self.e-mail=e-mail
+	self.telefono=telefono
+	self.celular=celular
+        self.rfc=rfc
+	self.direccion=direccion
+	
+	
+	
