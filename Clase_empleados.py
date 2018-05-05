a7a30fc896336b5d1966e3d80f309c5ae2517c56
yaxkin09 <36283613+yaxkin09@users.noreply.github.com>
2018-05-05 13:53:01 -0500

    Add files via upload (#2)

---
+++ Clase_empleados.py
@@ -0,0 +1,16 @@
+import Clase_Persona
+import Clase_Usuarios
+
+class Empleado(Persona):
+    Puesto
+    Jornada
+    Sueldo
+    Permisos
+    
+    def __init__():
+        self.Puesto=Puesto
+        self.Jornada=Jornada
+        self.Sueldo=Sueldo
+        self.Permisos=Permisos
+
+    
