f3f99ed1c0d31e4ec9335660b51e06d203ec3d38
yaxkin09 <36283613+yaxkin09@users.noreply.github.com>
2018-05-05 13:50:09 -0500

    Add files via upload

---
+++ Clase_Contador.py
@@ -0,0 +1,14 @@
+#Propuesta de clase para el módulo Facturas
+import Clase_Persona
+import Clase_Usuario
+import Clase_Empleados
+import Clase_Facturas
+
+class Contador(Empleado):
+    
+    
+    def __init__(self,ID,Nombre,Apellidos,Direccion,Telefono,Edad,Correo,RFC,Contraseña,COdPos,Puesto,Horario,Sueldo):
+        pass
+        
+    def Gen_factura():
+        
---
+++ Clase_RecHum.py
@@ -0,0 +1,20 @@
+import Clase_Persona
+import Clase_Empleados
+import Main_RecHum
+import Contratacion_RecHum
+import 
+
+class RecHuman(Empleado):
+    
+    
+    def __init__(self,ID,Nombre,Apellidos,Direccion,Telefono,Edad,Correo,RFC,Contraseña,COdPos,Puesto,Horario,Sueldo,Permisos=1):
+               
+    
+    def Save(self):
+        
+
+    def Edit(self):
+       
+        
+    def Delete(self):
+        
