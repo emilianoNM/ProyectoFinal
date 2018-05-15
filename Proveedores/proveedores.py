6efea1d07c20a185d8834b737ced1e2d32822ffd
alvert1996 <36251909+alvert1996@users.noreply.github.com>
2018-05-14 22:27:11 -0500

    Update proveedores.py

--- Proveedores/proveedores.py
+++ Proveedores/proveedores.py
@@ -1,23 +1,24 @@
 
+import proveedores_consola
 import pedidos
 import entregas_programadas
 import pickle
 import time
 import os
 import uuid
+import json
 
 class Proveedores():
+    numero_p=None
     nombre_p=None
     logo=None
     razon_social=None
     telefono_p=None
     correo_p=None
     direccion_p=None
-    
-    Permisos=None
-    f_escritura=open("proveedores1.txt","wb")
-    Proveedores_lista=[]
-    def __init__(self,nombre_p,logo,razon_social,telefono_p,correo_p,direccion_p):
+
+    def __init__(self,numero_p,nombre_p,logo,razon_social,telefono_p,correo_p,direccion_p):
+        self.numero_p=numero_p
         self.nombre_p=nombre_p
         self.logo=logo
         self.razon_social=razon_social
@@ -26,23 +27,46 @@ class Proveedores():
         self.direccion_p=direccion_p
     
 
-    def load(self):
-        b_lectura=open("proveedores1.txt","rb")
-        c=pickle.load(b_lectura)
-        print("elementos en archivo= "+str((len(c))))
-        b_lectura.close()
+    def load(numero_pp):
+        print (numero_pp)
+        b_lectura=open("Proveedores.dat",'r')
+        c=b_lectura.read()
+        listaJson=json.loads(c)
+
+        print ("buscando..")
+        numeroPP=listaJson[numero_pp]['numero_p']
+
+        return numeroPP
+
+        #for numero_pp in listaJson:
+           # if  numero_pp==listaJson("listaJson"):
+            #    numeroPP=listaJson[numero_pp]['numero_p']
+             #   nombrePP=listaJson[numero_pp]['nombre_p']
+              #  logoPP=listaJson[numero_pp]['logo']
+               # razonSocialP=listaJson[numero_pp]['razon_social']
+               # TelefonoP=listaJson[numero_pp]['telefono_p']
+               # CorreoP=listaJson[numero_pp]['correo_p']
+               # DireccionP=listaJson[numero_pp]['direccion_p']
+
+                #return(numeroPP,nomprePP,logoPP,razonSocialP,TelefonoP,CorreoP,DireccionP)
 
     def save(self):
-        Proveedores.Proveedores_lista.append(self)
+        listaJson={}
+        
+        #convierte a Json
+        jsone=self.__dict__
+        print(jsone['nombre_p'])
 
-    def close(self):
-        pickle.dump(Proveedores.Proveedores_lista,self.f_escritura)
-        self.f_escritura.close()
+        #Crea lista desde Json
+        listaJson[jsone['numero_p']]=jsone
+        print (listaJson)
 
-    def Search(snompre_p):
-        for p in Proveedores.Proveedores_lista:
-            if(snompre_p==nombre_pp):
-                 return p
+        lista2=open("Proveedores.dat",'w')
+        listacom=json.dumps(listaJson)
+        print ("lista guardada")
+        lista2.write(listacom)
+        lista2.close 
+        pass
 
     def validar_pedido(numero_pedido_e):
         vp_lectura=open("pedidosp.txt","rb")
@@ -51,11 +75,15 @@ class Proveedores():
             if(numero_pedido==numero_pedido_e):
                 return vp
 
-        
+def load(numero_pp):
+    print (numero_pp)
+    b_lectura=open("Proveedores.dat",'r')
+    c=b_lectura.read()
+    print (c )
+    print("\n")
+    listaJsonS=json.loads(c)
+    pp=Proveedores.nombre_p
+    print(pp)
+    print (listaJsonS['pp'])
 
-
-#f=open("proveedores.txt","w")
-#f.write(s1+"\n")
-#f.write(str(proveedor2))
-#f.close()
-
+    print ("buscando..")
