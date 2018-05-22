0cb7c0dcb2b954eec40848571960ac957585937d
Eduardo36 <36463790+Eduardo36@users.noreply.github.com>
2018-05-21 19:17:54 -0500

    Add files via upload

---
+++ Facturas/Clientes.py
@@ -0,0 +1,16 @@
+class Cliente:
+	Nombre = None
+	Direccion= None
+	Correo = None
+	RFC = None
+	Telefono=None
+	
+	def _init_(self, Nombre, Direccion, Correo, RFC,Telefono):
+		self.Nombre = Nombre
+		self.Direccion= Direccion
+		self.Correo = Correo
+		self.RFC = RFC
+		self.Telefono=Telefono
+ 
+		
+	
