9a698f7d1f1ce60c75dd7de3797d30ceecf81da8
alvert1996 <36251909+alvert1996@users.noreply.github.com>
2018-05-28 12:57:25 -0500

    Add files via upload
    La ventana principal botones controla a las otras dos, y funcionan con el archivo proveedores

---
+++ Proveedores_Frame/proveedores.py
@@ -0,0 +1,59 @@
+import pickle
+
+import json
+
+class Proveedores():
+    numero_p=None
+    nombre_p=None
+    logo=None
+    razon_social=None
+    telefono_p=None
+    correo_p=None
+    direccion_p=None
+
+    def __init__(self,numero_p,nombre_p,logo,razon_social,telefono_p,correo_p,direccion_p):
+        self.numero_p=numero_p
+        self.nombre_p=nombre_p
+        self.logo=logo
+        self.razon_social=razon_social
+        self.telefono_p=telefono_p
+        self.correo_p=correo_p
+        self.direccion_p=direccion_p
+
+    def save(self):
+        listaJson={}
+
+        lista=open("Proveedores.dat",'r')
+        lisJson=lista.read()
+        listaJson=json.loads(lisJson)
+        
+        #convierte a Json
+        jsone=self.__dict__
+        print (jsone)
+        
+        #Crea lista desde Json
+        listaJson[jsone['numero_p']]=jsone
+
+        lista2=open("Proveedores.dat",'w')
+        listacom=json.dumps(listaJson)
+        print("Nuevo Proveedor agregado")
+        print(listacom)
+        lista2.write(listacom)
+        lista2.close()
+
+    def validar_pedido(numero_pedido_e):
+        vp_lectura=open("pedidosp.txt","rb")
+        avp=pickle.load(vp_lecutra)
+        for vp in pedidos.pedidos_Lista:
+            if(numero_pedido==numero_pedido_e):
+                return vp
+
+    def load(numero_pp):
+        listaJsonS={}
+        b_lectura=open("Proveedores.dat",'r')
+        c=b_lectura.read()
+        listaJsonS=json.loads(c)
+
+        print ("buscando..")
+
+        return listaJsonS[numero_pp]['nombre_p']
---
+++ Proveedores_Frame/ventana_principal_botones.py
@@ -0,0 +1,51 @@
+import tkinter 
+import interfaz_guardar_proveedores_conexion
+import interfaz_buscar_proveedores_conexion
+
+from tkinter import *
+
+class Principal():
+
+    def __init__(self,master):
+        self.master=master
+        self.master.title("Proveedores")
+
+        self.titulo=Frame(self.master,width=500,height=400)
+        self.titulo.pack(fill="y")
+        self.titulo1=Label(self.titulo,text="MENU PROVEEDORES",font=(80))
+        self.titulo1.grid(row=1,column=1)
+        self.titulo2=Label(self.titulo,text="ELIGE UNA OPCIÓN",font=(80))
+        self.titulo2.grid(row=2,column=1)
+        self.titulo.config(bd=10)
+        self.titulo.config(relief="groove")
+
+        self.boton=Frame(self.master,width=500,height=400)
+        self.boton.pack(fill="y")
+        self.Agregar=Button(self.boton,command=self.save,text="Agregar Proveedor",background="white")
+        self.Agregar.grid(row=3,column=1)
+        self.Buscar=Button(self.boton,command=self.load,text="Buscar Proveedor",background="white")
+        self.Buscar.grid(row=3,column=5)
+        self.Pedido=Button(self.boton,text="Validar Pedido",background="white")
+        self.Pedido.grid(row=4,column=1)
+        self.Fecha=Button(self.boton,text="Agregar Fecha",background="white")
+        self.Fecha.grid(row=4,column=5)
+
+    def save(self):
+
+        root2=Toplevel(self.master)
+        guar=interfaz_guardar_proveedores_conexion.ventanaguardar(root2)
+        
+
+    def load(self):
+        root3=Toplevel(self.master)
+        car=interfaz_buscar_proveedores_conexion.ventanabuscar(root3)
+        
+
+
+def main():
+    root=Tk()
+    ventana=Principal(root)
+    root.mainloop()
+
+if __name__ == '__main__':
+    main()
